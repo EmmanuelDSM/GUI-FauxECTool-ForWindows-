2e2f9cdedfcbaf673c2531cdc56310b6a6edbfc5
Emmanuel DSM <125594743+EmmanuelDSM@users.noreply.github.com>
2023-09-27 23:20:50 +0200

    Ajout de simple fichier de test

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main (){
+
+    int a = 900;
+    int b = 250;
+
+    int sum = a + b;
+
+    std::cout << sum << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << sum << std::endl;
+    return 0;
+
+
+
+}
